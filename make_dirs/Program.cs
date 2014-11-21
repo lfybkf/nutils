d0fda920bac95208c716724bf05de95c2df2a061
unknown <bulychev.d@tD94DE8021B6BC.aetp.nn>
2014-11-21 11:45:20 +0300

    init

---
+++ make_dirs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int LIMIT = 70;
+        
+        static void Main(string[] args)
+        {
+            string baseDir = Environment.CurrentDirectory;
+            string prefix = baseDir.Split(Path.DirectorySeparatorChar).Last();
+            string newDir = null;
+            var files = Directory.GetFiles(baseDir);
+
+            int portions = files.Length / LIMIT;
+            for (int i = 0; i <= portions; i++)
+            {
+                newDir = string.Format("{0}_{1:D3}", prefix, i);
+                Directory.CreateDirectory(newDir);
+                var onePortionOfFiles = files.Skip(i * LIMIT).Take(LIMIT);
+                foreach (var file in onePortionOfFiles)
+                {
+                    File.Move(file, Path.Combine(baseDir, newDir, Path.GetFileName(file)));
+                }//for
+            }//for
+
+            Console.ReadLine();
+        }//function
+    }//class
+}
