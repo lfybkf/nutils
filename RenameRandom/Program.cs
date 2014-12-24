163ba4c3d514fca02422e071eacf25722944fdd6
unknown <bulychev.d@tD94DE8021B6BC.aetp.nn>
2014-12-24 12:56:59 +0300

    now renrand

---
+++ RenameRandom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "RenameRandom";
+            string baseDir = Environment.CurrentDirectory;
+            string prefix = baseDir.Split(Path.DirectorySeparatorChar).Last();
+            Random rand = new Random();
+            string newName;
+            foreach (var file in Directory.GetFiles(baseDir))
+            {
+                newName = string.Format("{0}_{1:D7}{2}", prefix, rand.Next(9999999), Path.GetExtension(file));
+                //newName = Path.GetRandomFileName();
+                if (File.Exists(newName) == false && Path.GetFileNameWithoutExtension(file) != myName )
+                    File.Move(file, newName);
+            }//for
+        }//function
+    }
+}
