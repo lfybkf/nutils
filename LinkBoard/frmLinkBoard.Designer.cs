6cbed6ad4f28c9dc5f23680dd5c3178658b33bd6
lfybkf <lfybkf@mail.ru>
2016-01-31 11:16:42 +0300

    now lins

--- LinkBoard/frmLinkBoard.Designer.cs
+++ LinkBoard/frmLinkBoard.Designer.cs
@@ -28,12 +28,38 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.listLinks = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// listLinks
+			// 
+			this.listLinks.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listLinks.FormattingEnabled = true;
+			this.listLinks.ItemHeight = 20;
+			this.listLinks.Location = new System.Drawing.Point(0, 0);
+			this.listLinks.Name = "listLinks";
+			this.listLinks.Size = new System.Drawing.Size(750, 410);
+			this.listLinks.TabIndex = 0;
+			// 
+			// frmLinkBoard
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(750, 410);
+			this.Controls.Add(this.listLinks);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "frmLinkBoard";
+			this.Text = "LinkBoard";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmLinkBoard_FormClosed);
+			this.Load += new System.EventHandler(this.frmLinkBoard_Load);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListBox listLinks;
 	}
 }
 
--- LinkBoard/frmLinkBoard.cs
+++ LinkBoard/frmLinkBoard.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,110 @@ namespace LinkBoard
 {
 	public partial class frmLinkBoard : Form
 	{
+		static List<string> links = new List<string>();
+		static readonly string http = @"http://";
+		static readonly string path = System.IO.Path.Combine(
+			Environment.CurrentDirectory, "LinkBoard.txt");
+
+		[DllImport("User32.dll")]
+		protected static extern int SetClipboardViewer(int hWndNewViewer);
+		// Добавляет новое окно в цепочку окон уведомляемых сообщением wm_DrawClipboard 
+		// пpи изменении буфеpа
+		// hWind указатель на окно
+		// Возвращает указатель на следующие окно в цепочке
+		
+		[DllImport("User32.dll", CharSet = CharSet.Auto)]
+		public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+		// Удаляет окно hWindDel из цепочки просмотра буфера изаменяет его hWindNext
+		// Возвращает true если окно найдено и удалено
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+		// Посылает сообщение окну
+		// hWind указатель на окно, msg код сообщения, 
+		// wparam1 и lparam дополнительная информация о сообщении
+		// Возвращает значение переданое функцией окна
+
+		IntPtr nextClipboardViewer; //Для хранения указателя на следующее окно
+
 		public frmLinkBoard()
 		{
 			InitializeComponent();
+			nextClipboardViewer = (IntPtr)SetClipboardViewer((int)this.Handle);
+		}//func
+
+		
+
+		private void frmLinkBoard_Load(object sender, EventArgs e)
+		{
+			listLinks.DataSource = links;
+			
+		}//func
+
+		private void frmLinkBoard_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			ChangeClipboardChain(this.Handle, nextClipboardViewer);
+
+		}//func
+
+		// переопределяем метод
+		protected override void WndProc(ref System.Windows.Forms.Message m)
+		{
+			// defined in winuser.h
+			const int WM_DRAWCLIPBOARD = 0x308;
+			const int WM_CHANGECBCHAIN = 0x030D;
+
+			switch (m.Msg)
+			{
+				case WM_DRAWCLIPBOARD: //содержимое буфера изменилось можно работать
+					DisplayClipboardData();
+					SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+					break;
+
+				case WM_CHANGECBCHAIN: //одно из окон удаено из цепочки, нужно восстановить цепочку
+					if (m.WParam == nextClipboardViewer)// Если удаляемое окно это следующие окно в цепочке
+						nextClipboardViewer = m.LParam;//Следующим окном делаем окно идущее в цепочке за удаляемым
+					else
+						SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);// Посылаем сообщение этому окну
+					break;
+
+				default:
+					base.WndProc(ref m);
+					break;
+			}
 		}
-	}
-}
+
+		public void ListRefresh(ListBox lb)
+		{
+			var bc = lb.BindingContext[lb.DataSource];
+			(bc as CurrencyManager).Refresh();
+		}//function
+
+		void DisplayClipboardData()
+		{
+			try
+			{
+				IDataObject iData = new DataObject();
+				iData = Clipboard.GetDataObject();
+
+
+				if (iData.GetDataPresent(DataFormats.Text))
+				{
+					string s = (string)iData.GetData(DataFormats.Text);
+					this.Text = string.Format("{0}:{1}", DateTime.Now.ToShortTimeString(), s);
+					if (s.StartsWith(http))
+					{
+						links.Add(s);
+						ListRefresh(listLinks);
+						System.IO.File.WriteAllLines(path, links);
+					}//if
+				}//if
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.ToString());
+			}
+		}
+
+	}//class
+}//ns
