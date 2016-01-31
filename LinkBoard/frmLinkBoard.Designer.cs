e54eb23517a28d6e0698c11c1d4d03d0ab02d366
lfybkf <lfybkf@mail.ru>
2016-01-31 14:04:04 +0300

    now icon

--- LinkBoard/frmLinkBoard.Designer.cs
+++ LinkBoard/frmLinkBoard.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLinkBoard));
 			this.listLinks = new System.Windows.Forms.ListBox();
 			this.SuspendLayout();
 			// 
@@ -48,6 +49,7 @@
 			this.ClientSize = new System.Drawing.Size(750, 410);
 			this.Controls.Add(this.listLinks);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "frmLinkBoard";
 			this.Text = "LinkBoard";
