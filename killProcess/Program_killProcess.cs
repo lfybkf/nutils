928fadf61175eb4cecc257b41b63cd33fa28c546
bulychev.d <bulychev.d@TD94DE8021B6BC>
2017-08-11 13:02:28 +0300

    (bdb)

--- killProcess/Program_killProcess.cs
+++ killProcess/Program_killProcess.cs
@@ -4,11 +4,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using io = System.IO;
 
 namespace killProcess
 {
+	class Item { public int ID; public string Name;
+		public static int C = 0;
+		public Item(int id, string name) { ID = id;  Name = name; }
+	}
+
 	class Program_killProcess
 	{
+		static string pathToList = io.Directory.EnumerateFiles(Environment.CurrentDirectory)
+			.FirstOrDefault(f => f.StartsWith("killProcess") && !f.EndsWith("exe"));
+		static Item[] items = pathToList != null
+			? io.File.ReadAllLines(pathToList).Select(f => new Item(Item.C++, f)).ToArray()
+			: new Item[] { new Item(1, "chrome"), new Item(2, "skype") };
+
 		static void Main(string[] args)
 		{
 			Console.BackgroundColor = ConsoleColor.DarkGray;
@@ -20,9 +32,35 @@ namespace killProcess
 			}//if
 			else
 			{
+				foreach (var item in items)
+				{
+					Console.WriteLine($"{item.ID} - {item.Name}");
+				}//for
+
 				Console.Write("Program to kill: ");
+
 				string arg = Console.ReadLine();
-				kill(arg);
+				int i;
+				if (int.TryParse(arg, out i))
+				{
+					var item = items.FirstOrDefault(z => z.ID == i);
+					if (item != null)
+					{
+						Console.WriteLine($"killing: {item.Name}");
+						kill(item.Name);
+					}//if
+					else
+					{
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.WriteLine($"no item in list with ID={i}");
+					}//else
+				}//if
+				else
+				{
+					Console.WriteLine($"killing: {arg}");
+					kill(arg);
+				}//else
+				
 			}//else
 		}//function
 
